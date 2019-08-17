29831f3de3a9484b671ea4ee1aab76816158d98c
grkm95 <gorkem_aslan0@hotmail.com>
2019-08-17 15:46:33 +0300

    Java Space Game

---
+++ space_game_test/src/gameScreen.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class gameScreen extends JFrame {
+
+    public gameScreen(String title) throws HeadlessException {
+
+
+    }
+
+    public static void main(String[] args){
+
+        gameScreen screen = new gameScreen("Space War");
+
+        screen.setResizable(false);
+        screen.setFocusable(false);
+        screen.setSize(800,600);
+
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        game game = new game();
+
+        game.requestFocus();
+
+        game.addKeyListener(game);
+
+        game.setFocusable(true);
+
+        game.setFocusTraversalKeysEnabled(false);
+
+        screen.add(game);
+
+        screen.setVisible(true);
+
+
+    }
+}
