1b98867996d9b1ba20fd8d1037e1a28256c17215
Görkem Aslan <gorkem_aslan0@hotmail.com>
2021-06-16 14:39:04 +0300

    Update game.java

--- space_game_test/src/game.java
+++ space_game_test/src/game.java
@@ -17,8 +17,7 @@ import java.util.logging.Logger;
 
 
 class Ates{
-
-
+    
     private int x;
     private int y;
 
@@ -47,50 +46,33 @@ class Ates{
 
 public class game extends JPanel implements KeyListener, ActionListener {
 
-        Timer timer = new Timer(5,this);
-        private int gecen_sure = 0 ;
-        private int harcanan_ates = 0;
-
+        Timer timer = new Timer(5, this);
         private BufferedImage image;
-
         private ArrayList<Ates> atesler = new ArrayList<Ates>();
-
-        private int atesdirY = 1;
-        private int topX = 0;
-
-        private int topdirX = 2;
-        private int uzayGemisiX = 0;
-        private int dirUzayX = 20;
-
+        private int gecen_sure      = 0 ;
+        private int harcanan_ates   = 0;
+        private int atesdirY        = 1;
+        private int topX            = 0;
+        private int topdirX         = 2;
+        private int uzayGemisiX     = 0;
+        private int dirUzayX        = 20;
 
         public Boolean kontrolEt(){
-
-
             for (Ates ates : atesler){
-
-                if (new Rectangle(ates.getX(),ates.getY(),10,20).intersects(new Rectangle(topX,0,20,20))){
-
-                    return true;
-                }
+                if (new Rectangle(ates.getX(), ates.getY(), 10, 20).intersects(new Rectangle(topX, 0, 20, 20))) return true;
             }
-
             return false;
-
         }
 
 
     public game() {
-
         try {
             image = ImageIO.read(new FileImageInputStream(new File("space_ship.png")));
         } catch (IOException ex) {
-            Logger.getLogger(game.class.getName()).log(Level.SEVERE,null,ex);
+            Logger.getLogger(game.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         setBackground(Color.BLACK);
-
         timer.start();
-
     }
 
 
@@ -98,44 +80,30 @@ public class game extends JPanel implements KeyListener, ActionListener {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-
         gecen_sure += 5 ;
-
         g.setColor(Color.red);
-        g.fillOval(topX,0,20,20);
-        g.drawImage(image,uzayGemisiX,490,image.getWidth()/10,image.getHeight()/10,this);
-
+        g.fillOval(topX, 0, 20, 20);
+        g.drawImage(image, uzayGemisiX, 490, image.getWidth() / 10, image.getHeight() / 10, this);
 
         for (Ates ates : atesler){
 
-            if (ates.getY() < 0 ){
-
-                atesler.remove(ates);
-            }
+            if (ates.getY() < 0 ) atesler.remove(ates);
         }
-
-
+        
         g.setColor(Color.BLUE);
-
+        
         for (Ates ates : atesler){
-
             g.fillRect(ates.getX(),ates.getY(),10,20);
         }
 
         if (kontrolEt()){
-
             timer.stop();
             String message = "Kazandınız...\n"+
                     "Harcanan Ateş : " + harcanan_ates +
-                    "\nGeçen Süre : " + gecen_sure/1000.0;
-            JOptionPane.showMessageDialog(this,message);
+                    "\nGeçen Süre : " + gecen_sure / 1000.0;
+            JOptionPane.showMessageDialog(this, message);
             System.exit(0);
         }
-
-
-
-
-
     }
 
     @Override
@@ -147,20 +115,13 @@ public class game extends JPanel implements KeyListener, ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         for (Ates ates : atesler){
-
-            ates.setY(ates.getY()-atesdirY);
-
+            ates.setY(ates.getY() - atesdirY);
         }
+        
         topX += topdirX;
 
-        if (topX >= 750){
-
-            topdirX = -topdirX;
-        }
-        if (topX <=0){
-
-            topdirX = -topdirX;
-        }
+        if (topX >= 750) topdirX = -topdirX;
+        if (topX <=0) topdirX = -topdirX;
         repaint();
 
     }
@@ -175,39 +136,17 @@ public class game extends JPanel implements KeyListener, ActionListener {
 
         int c = e.getKeyCode();
         if (c == KeyEvent.VK_LEFT){
-
-            if (uzayGemisiX <= 0 ){
-
-
-                uzayGemisiX = 0;
-            }
-            else {
-
-                uzayGemisiX -=dirUzayX;
-            }
-
+            if (uzayGemisiX <= 0 )uzayGemisiX = 0;
+            else uzayGemisiX -=dirUzayX;
         }
         else if (c == KeyEvent.VK_RIGHT){
-
-            if (uzayGemisiX >=720){
-                uzayGemisiX = 720;
-            }
-            else {
-
-                uzayGemisiX += dirUzayX;
-            }
-
+            if (uzayGemisiX >=720) uzayGemisiX = 720;
+            else uzayGemisiX += dirUzayX;
         }
-
         else if (c == KeyEvent.VK_SPACE){
-
             atesler.add(new Ates(uzayGemisiX+15,470));
-
-
             harcanan_ates++;
-
         }
-
     }
 
     @Override
